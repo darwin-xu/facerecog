507b3a02d33e37c8a5a01be711664de723cfe65e
Rocky Wan <rocky.wan@ericsson.com>
2018-03-04 13:20:19 +0800

    add interface definition

--- app.py
+++ app.py
@@ -5,24 +5,32 @@ from test_img import load_model, test_image
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    # return __file__
-    return os.path.dirname(os.path.realpath(__file__))
-
-@app.route('/exec/<string:cmd>', methods=['GET'])
-def my_exec(cmd):
-    # return __file__
-    result = eval("os.path.dirname(os.path.realpath(__file__))")
-    print (result)
-    return result
-
 @app.route('/face/<string:img_path>', methods=['GET'])
 def recong_face(img_path):
-    newimgpath = test_image(model, sess, graph, class_names, pnet, rnet, onet, img_path)
-    return newimgpath
+    # newimgpath = test_image(model, sess, class_names, pnet, rnet, onet, img_path)
+    return img_path#newimgpath
 
-model, sess, graph, class_names, pnet, rnet, onet = load_model('models/facenet/20180220-152437', '../models/my_classifier.pkl')
+@app.route('/detectFacesC', methods=['POST'])
+def detect_face_c():
+    return ""
+
+@app.route('/detectFacesD', methods=['POST'])
+def detect_face_d():
+    return ""
+
+@app.route('/registerFace/<string:id>', methods=['POST'])
+def register_face(id):
+    return ""
+
+@app.route('/classifyFace/<string:id>', methods=['POST'])
+def classify_face(id):
+    return ""
+
+@app.route('/removeFace/<string:id>', methods=['DELETE'])
+def remove_face(id):
+    return ""
+
+# model, sess, class_names, pnet, rnet, onet = load_model('models/facenet/20180220-152437', '../models/my_classifier.pkl')
 
 if __name__ == '__main__':
     app.run(debug=True)
