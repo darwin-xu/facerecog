5c8ef31425fa39657e36c6165c86716267ba2991
rockywan <rockywanyaho@gmail.com>
2018-03-05 23:00:27 +0800

    classifier ok

--- make_classifier.py
+++ make_classifier.py
@@ -13,16 +13,35 @@ import math
 import pickle
 from sklearn.svm import SVC
 
-def make_classifier(sess, graph, emb_array, classifier_filename):
+def make_classifier(sess, graph, emb_dict, classifier_filename):
+    # print('FKFKFKFKFKFKKFKFKFKFKFKFKFKFKKFKFKFKFKFKFKKFKFKFKKFKFKFKFKKFKFKFKKFKFKFK')
     classifier_filename_exp = os.path.expanduser(classifier_filename)
+
+    nrof_embs = 0 
+    labels = []
+    emb_size = 0
+    for emb in emb_dict.values():
+        nrof_embs += len(emb)
+        emb_size = len(emb[0])
+
+    i = 0
+    emb_array = np.zeros((nrof_embs, emb_size))
+    for label, embs in emb_dict.items():
+        for emb in embs:
+            labels.append(label)
+            emb_array[i, :] = emb
+            i = i + 1
 
     # Train classifier
     print('Training classifier')
     model = SVC(kernel='linear', probability=True)
-    model.fit(emb_array.values(), emb_array.keys())
+    model.fit(emb_array, labels)
 
+    classes = list(emb_dict.keys())
     # Saving classifier model
     with open(classifier_filename_exp, 'wb') as outfile:
-        pickle.dump((model, emb_array.keys()), outfile)
+        pickle.dump((model, classes), outfile)
     print('Saved classifier model to file "%s"' % classifier_filename_exp)
     print('Goodluck')
+
+    return model, classes
