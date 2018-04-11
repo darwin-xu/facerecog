25bd1987755fba3a86ec9bcda0342432d49bd185
darwin-xu <xu.dai.wen@126.com>
2018-04-12 01:25:42 +0800

    DEV: change the detect method

--- test_client/detectVideo.py
+++ test_client/detectVideo.py
@@ -9,50 +9,36 @@ detect_uri = '/detectFacesD'
 
 cap = cv2.VideoCapture(0)
 
-delay = 2000
+detecting = False
 
-result = None
-last = int(round(time.time() * 1000))
 while (True):
-    now = int(round(time.time() * 1000))
-
     # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    #frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)  #resize frame (optional)
+    _, frame = cap.read()
 
     key = cv2.waitKey(1)
-    if key & 0xFF == ord('c'):
-        last = now
-        m1 = int(round(time.time() * 1000))
-        jpg = cv2.imencode('.jpg', frame)[1].tostring()
-        files = {'file': jpg}
-        response = requests.post(base_uri + detect_uri, files=files)
-        m2 = int(round(time.time() * 1000))
-        print("get response cost: " + str(m2 - m1) + " milliseconds")
 
-        if response.ok:
-            result = json.loads(response.content.decode('utf-8'))
-
+    if key & 0xFF == ord('d'):
+        detecting = True
+    elif key & 0xFF == ord('s'):
+        detecting = False
     elif key & 0xFF == ord('q'):
         break
 
-    if now - last > delay:
-        result = None
+    if detecting:
+        jpg = cv2.imencode('.jpg', frame)[1].tostring()
+        files = {'file': jpg}
+        response = requests.post(base_uri + detect_uri, files=files)
+        if response.ok:
+            result = json.loads(response.content.decode('utf-8'))
+            for f in result[0]["faces"]:
+                cv2.rectangle(frame, (f["x1"], f["y1"]), (f["x2"], f["y2"]),
+                              (0, 0, 255), 2)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                # Add name on top of the rectangle
+                cv2.putText(frame, f["id"], (f["x1"], f["y2"] + 30), font, 1,
+                            (255, 255, 255), 1, cv2.LINE_AA)
 
-    if result != None:
-        for f in result[0]["faces"]:
-            cv2.rectangle(frame, (f["x1"], f["y1"]), (f["x2"], f["y2"]),
-                          (0, 0, 255), 2)
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            # Add name on top of the rectangle
-            cv2.putText(frame, f["id"], (f["x1"], f["y2"] + 30), font, 1,
-                        (255, 255, 255), 1, cv2.LINE_AA)
-
-    # Display the resulting frame
     cv2.imshow('frame', frame)
-    # i += 1
-    # cv2.imwrite('out/messigray' + str(i) + '.jpg', frame)
 
 # When everything done, release the capture
 cap.release()
