fb33b1228afd8164df8c6e81ad26e9411a4a7045
darwin-xu <xu.dai.wen@126.com>
2018-04-18 23:35:26 +0800

    DEV: add input for test

--- test10.py
+++ test10.py
@@ -7,6 +7,8 @@ from scipy import misc
 import cv2
 import detect_face
 
+show = False
+
 graph = tf.get_default_graph()
 with graph.as_default():
     sess = tf.Session()
@@ -20,14 +22,14 @@ with graph.as_default():
         shrink = 1.0
         step = 0.1
         minsize = 20
+        factor = 0.709
 
         while True:
             image = cv2.imread(sys.argv[1])
             forshow = misc.imresize(image, 0.5, interp='bilinear')
 
             threshold = [pt, rt, ot]
-            print("threshold", threshold, "shrink", shrink, "minsize", minsize)
-            factor = 0.709
+            print("threshold", threshold, "shrink", shrink, "minsize", minsize, "factor", factor)
             sys.stdout.flush()
 
             thumbnail = misc.imresize(
@@ -52,35 +54,48 @@ with graph.as_default():
                 y2 = int(min(y2, size[0]))
                 cv2.rectangle(forshow, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
-            cv2.imshow("image", forshow)
+            if show:
+                cv2.imshow("image", forshow)
 
-            key = cv2.waitKey(0) & 0xFF
+                key = cv2.waitKey(0) & 0xFF
 
-            if key == ord('a'):
-                pt -= step
-            elif key == ord('z'):
-                pt += step
-            elif key == ord('s'):
-                rt -= step
-            elif key == ord('x'):
-                rt += step
-            elif key == ord('d'):
-                ot -= step
-            elif key == ord('c'):
-                ot += step
-            elif key == ord('f'):
-                shrink -= step
-            elif key == ord('v'):
-                shrink += step
-            elif key == ord('g'):
-                minsize -= 10
-            elif key == ord('b'):
-                minsize += 10
-            elif key == ord('q'):
-                quit()
-            elif key == ord('r'):
-                pass
+                if key == ord('a'):
+                    pt -= step
+                elif key == ord('z'):
+                    pt += step
+                elif key == ord('s'):
+                    rt -= step
+                elif key == ord('x'):
+                    rt += step
+                elif key == ord('d'):
+                    ot -= step
+                elif key == ord('c'):
+                    ot += step
+                elif key == ord('f'):
+                    shrink -= step
+                elif key == ord('v'):
+                    shrink += step
+                elif key == ord('g'):
+                    minsize -= 10
+                elif key == ord('b'):
+                    minsize += 10
+                elif key == ord('h'):
+                    factor -= 0.1
+                elif key == ord('n'):
+                    factor += 0.1
+                elif key == ord('q'):
+                    quit()
+                elif key == ord('r'):
+                    pass
 
-            sys.stdout.flush()
+                cv2.destroyAllWindows()
 
-            cv2.destroyAllWindows()
+            else:
+                i = input("pt, rt, ot, shrink, minsize, factor:")
+                pt, rt, ot, shrink, minsize, factor = i.split()
+                pt = float(pt)
+                rt = float(rt)
+                ot = float(ot)
+                shrink = float(shrink)
+                minsize = int(minsize)
+                factor = float(factor)
