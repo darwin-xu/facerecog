a155a4894949d8cb066bd7a0c5f09518342ef6ce
darwin-xu <xu.dai.wen@126.com>
2018-05-14 01:25:36 +0800

    DEV: ready for internet images

--- utility/face_extractor.py
+++ utility/face_extractor.py
@@ -1,4 +1,8 @@
-#This is a tool to extractor faces from the picture.
+# This is a tool to extractor faces from the pictures.
+# After save the html, a bunch of files contain faces saved in the folder.
+# For instance: <download/images/liudehua>
+# This tool will extract the faces and saved into another folder
+# For instance: <download/labeled_faces/liudehua>
 
 import os
 import sys
@@ -14,103 +18,60 @@ from scipy import misc, ndimage
 import detect_face
 
 
-def sharpen(image, factor):
-    im = PIL.Image.fromarray(image)
-    enhancer = ImageEnhance.Sharpness(im)
-    return np.asarray(enhancer.enhance(factor))
+def getDestPathAndFilename(filename, destName):
+    folder = os.path.dirname(filename)
+    individual = os.path.basename(folder)
+    download = os.path.dirname(os.path.dirname(folder))
+    dest = os.path.join(download, destName, individual)
+    name = os.path.splitext(os.path.basename(filename))[0]
+    return (dest, name)
 
 
-image = imageio.imread(sys.argv[1])
-imageSize = image.shape[0:2]
-path = os.path.dirname(sys.argv[1])
-fileName = os.path.splitext(os.path.basename(sys.argv[1]))[0]
-
-graph = tf.get_default_graph()
-with graph.as_default():
-    sess = tf.Session()
-    with sess.as_default():
-        pnet, rnet, onet = detect_face.create_mtcnn(sess,
-                                                    '../facenet/src/align')
-
-        minsize = 20
-        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
-        factor = 0.709  # scale factor
-        bounding_boxes, _ = detect_face.detect_face(image, minsize, pnet, rnet,
-                                                    onet, threshold, factor)
-
-        pathForFaces = os.path.join(path, fileName)
-        if not os.path.exists(pathForFaces):
-            os.makedirs(pathForFaces)
-
+def extractFace(imageName, params):
+    # Create the folder to save the face images.
+    path, filename = getDestPathAndFilename(imageName, 'labeled_faces')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    try:
+        image = imageio.imread(imageName)
+        imageSize = image.shape[0:2]
+        bounding_boxes, _ = detect_face.detect_face(
+            image, params[0], params[3], params[4], params[5], params[1],
+            params[2])
         i = 0
-        show = False
-        f = 4
         for box in bounding_boxes:
             x1 = int(max(box[0], 0))
             y1 = int(max(box[1], 0))
             x2 = int(min(box[2], imageSize[1]))
             y2 = int(min(box[3], imageSize[0]))
             face = image[y1:y2, x1:x2, :]
-            outImageName = os.path.join(path, fileName,
-                                        fileName + '_' + str(i) + '.png')
-            print(outImageName)
-            imageio.imsave(outImageName, face)
-
-            # #if not show:
-            # plt.subplot(2, 6, 1)
-            # plt.imshow(face)
-
-            # face1 = misc.imresize(
-            #     face, ((y2 - y1) * 2, (x2 - x1) * 2), interp='bilinear')
-            # plt.subplot(2, 6, 2)
-            # plt.imshow(face1)
-            # plt.title('bilinear')
-
-            # face1 = sharpen(face1, f)
-            # plt.subplot(2, 6, 8)
-            # plt.imshow(face1)
-
-            # face1 = misc.imresize(
-            #     face, ((y2 - y1) * 2, (x2 - x1) * 2), interp='nearest')
-            # plt.subplot(2, 6, 3)
-            # plt.imshow(face1)
-            # plt.title('nearest')
-
-            # face1 = sharpen(face1, f)
-            # plt.subplot(2, 6, 9)
-            # plt.imshow(face1)
-
-            # face1 = misc.imresize(
-            #     face, ((y2 - y1) * 2, (x2 - x1) * 2), interp='lanczos')
-            # plt.subplot(2, 6, 4)
-            # plt.imshow(face1)
-            # plt.title('lanczos')
-
-            # face1 = sharpen(face1, f)
-            # plt.subplot(2, 6, 10)
-            # plt.imshow(face1)
-
-            # face1 = misc.imresize(
-            #     face, ((y2 - y1) * 2, (x2 - x1) * 2), interp='bicubic')
-            # plt.subplot(2, 6, 5)
-            # plt.imshow(face1)
-            # plt.title('bicubic')
-
-            # face1 = sharpen(face1, f)
-            # plt.subplot(2, 6, 11)
-            # plt.imshow(face1)
-
-            # face1 = misc.imresize(
-            #     face, ((y2 - y1) * 2, (x2 - x1) * 2), interp='cubic')
-            # plt.subplot(2, 6, 6)
-            # plt.imshow(face1)
-            # plt.title('cubic')
-
-            # face1 = sharpen(face1, f)
-            # plt.subplot(2, 6, 12)
-            # plt.imshow(face1)
-
-            # show = True
-            # plt.show()
-
+            outImageName = os.path.join(path, filename + '_' + str(i) + '.png')
             i += 1
+            print('Save to' + outImageName)
+            imageio.imsave(outImageName, face)
+    except ValueError:
+        pass
+
+
+# Get the filenames in the folder
+folder = os.path.abspath(sys.argv[1])
+filenames = [
+    os.path.join(folder, dir) for dir in os.listdir(folder)
+    if os.path.isfile(os.path.join(folder, dir))
+]
+
+# Create graph and sess for tensorflow
+graph = tf.get_default_graph()
+with graph.as_default():
+    sess = tf.Session()
+    with sess.as_default():
+        pnet, rnet, onet = detect_face.create_mtcnn(sess,
+                                                    '../facenet/src/align')
+        minsize = 20
+        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+        factor = 0.709  # scale factor
+        params = (minsize, threshold, factor, pnet, rnet, onet)
+
+        # For each file, extract the faces
+        for imageName in filenames:
+            extractFace(imageName, params)
