d092285c1365e21b53219102b2be19964c815e6f
darwin-xu <xu.dai.wen@126.com>
2018-05-17 01:51:32 +0800

    DEV: implement for enlargeBox

--- utility/face_extractor.py
+++ utility/face_extractor.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import unittest
 
 import imageio
 import matplotlib.pyplot as plt
@@ -27,41 +28,68 @@ def getDestPathAndFilename(filename, destName):
     return (dest, name)
 
 
+def enlargeBox(box, imageSize, enlarge):
+    # Get the width and height of the box
+    hExt = (box[2] - box[0]) * enlarge
+    vExt = (box[3] - box[1]) * enlarge
+
+    # Calculate the enlarged bound of the box, maybe cross the border of the image
+    x1 = int(box[0] - hExt / 2)
+    x2 = int(box[2] + hExt / 2)
+    y1 = int(box[1] - vExt / 2)
+    y2 = int(box[3] + vExt / 2)
+    print('x1,x2,y1,y2', x1, x2, y1, y2)
+
+    # Calculate the inner margin in the enlarge box
+    left = 0 if (0 <= x1) else (0 - x1)
+    right = (x2 - x1) if (x2 <=
+                          imageSize['width']) else (imageSize['width'] - x1)
+    top = 0 if (0 <= y1) else (0 - y1)
+    bottom = (y2 - y1) if (y2 <=
+                           imageSize['height']) else (imageSize['height'] - y1)
+
+    print('left, right, top, bottom', left, right, top, bottom)
+
+    # Adjust the clipping area
+    x1 = max(x1, 0)
+    y1 = max(y1, 0)
+    x2 = min(x2, imageSize['width'])
+    y2 = min(y2, imageSize['height'])
+
+    return {
+        'left': left,
+        'right': right,
+        'top': top,
+        'bottom': bottom,
+        'x1': x1,
+        'y1': y1,
+        'x2': x2,
+        'y2': y2
+    }
+
+
 def cutBoxFromImage(box, image, enlarge=2):
     """ Cut a box from a image with enlarge ratio.
         if the enlarged box is out of the bound of the image.
         random data will be filled.
     """
-    imageSize = image.shape[0:2]
 
-    # Get the width and height of the box
-    w = box[2] - box[0]
-    h = box[3] - box[1]
+    params = enlargeBox(box, {
+        'width': image.shape[0:2][1],
+        'height': image.shape[0:2][1]
+    }, enlarge)
 
-    # Calculate the enlarged bound of the box, maybe cross the border of the image
-    x1 = int(box[0] - w / 2)
-    x2 = int(box[2] + w / 2)
-    y1 = int(box[1] - h / 2)
-    y2 = int(box[3] + h / 2)
+    # Get the result out of the params
+    left, top, right, bottom = params['left'], params['top'], params[
+        'right'], params['bottom']
+    x1, y1, x2, y2 = params['x1'], params['y1'], params['x2'], params['y2']
 
     # Initial a random array
     img = np.random.rand(y2 - y1, x2 - x1, image.shape[2])
 
-    # Calculate the inner margin in the enlarge box
-    left = 0 if (0 - x1 >= 0) else (0 - x1)
-    right = (x2 - x1) if (imageSize[1] - x2 >= 0) else (y2 - y1 -
-                                                        (imageSize[1] - x2))
-    top = 0 if (0 - y1 >= 0) else (0 - y1)
-    bottom = (y2 - y1) if (imageSize[0] - y2 >= 0) else (y2 - y1 -
-                                                         (imageSize[0] - y2))
+    # Cut the face out of the image
+    img[top:bottom, left:right, :] = image[y1:y2, x1:x2, :]
 
-    # Adjust the clipping area
-    x1 = max(x1, 0)
-    y1 = max(y1, 0)
-    x2 = min(x2, imageSize[1])
-    y2 = min(y2, imageSize[0])
-
-    img[top:bottom, left:right, :] = image[y1:y2, x1:y2, :]
     return img
 
 
@@ -86,25 +114,77 @@ def extractFace(imageName, params):
         pass
 
 
-# Get the filenames in the folder
-folder = os.path.abspath(sys.argv[1])
-filenames = [
-    os.path.join(folder, dir) for dir in os.listdir(folder)
-    if os.path.isfile(os.path.join(folder, dir))
-]
+if __name__ == '__main__':
+    # Get the filenames in the folder
+    folder = os.path.abspath(sys.argv[1])
+    filenames = [
+        os.path.join(folder, dir) for dir in os.listdir(folder)
+        if os.path.isfile(os.path.join(folder, dir))
+    ]
 
-# Create graph and sess for tensorflow
-graph = tf.get_default_graph()
-with graph.as_default():
-    sess = tf.Session()
-    with sess.as_default():
-        pnet, rnet, onet = detect_face.create_mtcnn(sess,
-                                                    '../facenet/src/align')
-        minsize = 20
-        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
-        factor = 0.709  # scale factor
-        params = (minsize, threshold, factor, pnet, rnet, onet)
+    # Create graph and sess for tensorflow
+    graph = tf.get_default_graph()
+    with graph.as_default():
+        sess = tf.Session()
+        with sess.as_default():
+            pnet, rnet, onet = detect_face.create_mtcnn(
+                sess, '../facenet/src/align')
+            minsize = 20
+            threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+            factor = 0.709  # scale factor
+            params = (minsize, threshold, factor, pnet, rnet, onet)
 
-        # For each file, extract the faces
-        for imageName in filenames:
-            extractFace(imageName, params)
+            # For each file, extract the faces
+            for imageName in filenames:
+                extractFace(imageName, params)
+
+
+class TestFaceExtractor(unittest.TestCase):
+    def testEnlargeBox(self):
+        box = [100, 100, 150, 150]
+        imageSize = {'width': 250, 'height': 250}
+        params = enlargeBox(box, imageSize, enlarge=2)
+        self.assertEqual(params['left'], 0)
+        self.assertEqual(params['top'], 0)
+        self.assertEqual(params['right'], 150)
+        self.assertEqual(params['bottom'], 150)
+        self.assertEqual(params['x1'], 50)
+        self.assertEqual(params['y1'], 50)
+        self.assertEqual(params['x2'], 200)
+        self.assertEqual(params['y2'], 200)
+
+        box = [140, 210, 190, 290]
+        imageSize = {'width': 200, 'height': 300}
+        params = enlargeBox(box, imageSize, enlarge=2)
+        self.assertEqual(params['left'], 0)
+        self.assertEqual(params['top'], 0)
+        self.assertEqual(params['right'], 110)
+        self.assertEqual(params['bottom'], 170)
+        self.assertEqual(params['x1'], 90)
+        self.assertEqual(params['y1'], 130)
+        self.assertEqual(params['x2'], 200)
+        self.assertEqual(params['y2'], 300)
+
+        box = [20, 20, 100, 50]
+        imageSize = {'width': 200, 'height': 300}
+        params = enlargeBox(box, imageSize, enlarge=2)
+        self.assertEqual(params['left'], 60)
+        self.assertEqual(params['top'], 10)
+        self.assertEqual(params['right'], 240)
+        self.assertEqual(params['bottom'], 90)
+        self.assertEqual(params['x1'], 0)
+        self.assertEqual(params['y1'], 0)
+        self.assertEqual(params['x2'], 180)
+        self.assertEqual(params['y2'], 80)
+
+        box = [20, 20, 100, 50]
+        imageSize = {'width': 200, 'height': 300}
+        params = enlargeBox(box, imageSize, enlarge=2)
+        self.assertEqual(params['left'], 60)
+        self.assertEqual(params['top'], 10)
+        self.assertEqual(params['right'], 240)
+        self.assertEqual(params['bottom'], 90)
+        self.assertEqual(params['x1'], 0)
+        self.assertEqual(params['y1'], 0)
+        self.assertEqual(params['x2'], 180)
+        self.assertEqual(params['y2'], 80)
